b88ef562fd71913488603035e4dd82ae07c469b8
TheCyberArcher <62696671+TheCyberArcher@users.noreply.github.com>
2024-01-24 19:29:32 +0100

    Update Phone-tools.sh

--- Phone-tools.sh
+++ Phone-tools.sh
@@ -7,8 +7,8 @@ sleep 3
 git clone https://github.com/N0rz3/Inspector.git ~/.humint-tools/Inspector/
 pip install -r ~/.humint-tools/Inspector/requirements.txt --break-system-packages
 
-git clone https://github.com/spider863644/PhoneNumber-OSINT ~/.humint-tools/Inspector/phone-number-osint/
-pip install -r ~/.humint-tools/Inspector/phone-number-osint/requirements.txt --break-system-packages
+git clone https://github.com/spider863644/PhoneNumber-OSINT ~/.humint-tools/phone-number-osint/
+pip install -r ~/.humint-tools/phone-number-osint/requirements.txt --break-system-packages
 
 yay -S phoneinfoga
 
