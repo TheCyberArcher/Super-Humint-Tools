e0209441e376e4279cb54a837a4f2510c72688da
TheCyberArcher <62696671+TheCyberArcher@users.noreply.github.com>
2024-01-24 17:33:33 +0100

    Update google-tools.sh

--- google-tools.sh
+++ google-tools.sh
@@ -9,3 +9,7 @@ yay -S python3 python-pip git
 git clone https://github.com/Zarcolio/sitedorks.git ~/.humint-tools/sitedorks/
 cd ~/.humint-tools/sitedorks/
 bash install.sh
+
+pip install pipx
+pipx ensurepath
+pipx install ghunt
