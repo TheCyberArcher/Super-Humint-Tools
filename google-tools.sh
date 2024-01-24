d16cd5294e5fc8b3cb8e28d768b425e3d5b5eec6
TheCyberArcher <62696671+TheCyberArcher@users.noreply.github.com>
2024-01-24 17:59:43 +0100

    Update google-tools.sh

--- google-tools.sh
+++ google-tools.sh
@@ -9,4 +9,5 @@ yay -S python3 python-pip git pipx
 git clone https://github.com/Zarcolio/sitedorks.git ~/.humint-tools/sitedorks/
 pip install -r ~/.humint-tools/sitedorks/requirements.txt  --break-system-packages
 
-pipx install ghunt --break-system-packages
+pip install pipx  --break-system-packages
+pip install ghunt --break-system-packages
