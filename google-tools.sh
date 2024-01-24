e171b38529ec964a20fe28a51671c9213ce3eed3
TheCyberArcher <62696671+TheCyberArcher@users.noreply.github.com>
2024-01-24 17:56:04 +0100

    Update google-tools.sh

--- google-tools.sh
+++ google-tools.sh
@@ -4,11 +4,9 @@ echo "--- source : https://github.com/TheCyberArcher/Super-Humint-Tools/"
 
 Sleep 3
 
-yay -S python3 python-pip git
+yay -S python3 python-pip git pipx
 
 git clone https://github.com/Zarcolio/sitedorks.git ~/.humint-tools/sitedorks/
-pip install -r ~/.humint-tools/sitedorks/requirements.txt
+pip install -r ~/.humint-tools/sitedorks/requirements.txt  --break-system-packages
 
-pip install pipx --break-system-packages
-pipx ensurepath
 pipx install ghunt --break-system-packages
