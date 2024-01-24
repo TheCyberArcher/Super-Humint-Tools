d0ffaa59afc3531a743f5691a4062204dbbaf67a
TheCyberArcher <62696671+TheCyberArcher@users.noreply.github.com>
2024-01-24 19:27:33 +0100

    Create Phone-tools.sh

---
+++ Phone-tools.sh
@@ -0,0 +1,15 @@
+echo "--- Bienvenue sur Super-Humint-Tools (Phone-tools)---"
+
+echo "--- source : https://github.com/TheCyberArcher/Super-Humint-Tools/"
+
+sleep 3
+
+git clone https://github.com/N0rz3/Inspector.git ~/.humint-tools/Inspector/
+pip install -r ~/.humint-tools/Inspector/requirements.txt --break-system-packages
+
+git clone https://github.com/spider863644/PhoneNumber-OSINT ~/.humint-tools/Inspector/phone-number-osint/
+pip install -r ~/.humint-tools/Inspector/phone-number-osint/requirements.txt --break-system-packages
+
+yay -S phoneinfoga
+
+echo "--- Installation terminée ---"
