ca93a32a40592f9cb38745770a42d5105ea0c72c
TheCyberArcher <62696671+TheCyberArcher@users.noreply.github.com>
2024-01-24 17:46:44 +0100

    Update geoint.sh

--- geoint.sh
+++ geoint.sh
@@ -9,3 +9,7 @@ pip install -r ~/.humint-tools/geo-recon/requirements.txt --break-system-package
 
 git clone https://github.com/HunxByts/GhostTrack.git /.humint-tools/GhostTrack/
 pip install -r ~/.humint-tools/GhostTrack/requirements.txt --break-system-packages
+
+git clone https://github.com/r3mlab/tweetsmapper.git /.humint-tools/tweetsmapper/
+cd  ~/.humint-tools/GhostTrack/tweetsmapper/
+pip3 install -e
